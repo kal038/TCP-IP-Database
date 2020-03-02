b739bf649aa7e58ab84f5cf7e7befe4b5e2b7421
Parker Messner <pmm018@bucknell.edu>
2020-03-02 08:17:46 -0500

    Initial commit

---
+++ src/main/Main.java
@@ -0,0 +1,26 @@
+/* *****************************************
+ * CSCI205 - Software Engineering and Design
+ * Spring 2020
+ * Instructor: Prof. Chris Dancy
+ *
+ * Name: YOUR NAME
+ * Section: YOUR SECTION
+ * Date: 3/2/20
+ * Time: 8:10 AM
+ *
+ * Project: csci205_hw1
+ * Package: main
+ * Class: Main
+ *
+ * Description:
+ *
+ * ****************************************
+ */
+package
+        main;
+
+public class Main{
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
