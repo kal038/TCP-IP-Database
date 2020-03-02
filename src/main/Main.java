77ad187df83a22f37d631f4f49836e9a8d87ab35
Parker Messner <pmm018@bucknell.edu>
2020-03-02 08:54:39 -0500

    HW1,parker,csv reading in progress

--- src/main/Main.java
+++ src/main/Main.java
@@ -19,8 +19,52 @@
 package
         main;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main{
     public static void main(String[] args) {
         System.out.println("Hello World");
+
+    }
+
+    /**
+     * Reads in the CSV file and creates an array of workers
+     * @param fileName the name of the csv file
+     */
+    public void readInFile(String fileName){
+        final String separator = ",";
+        BufferedReader b = null;
+        try {
+            b = new BufferedReader(new FileReader(fileName));
+            String line;
+            while ((line = b.readLine()) != null) {
+                String[] separatedValues = line.split(separator);
+
+
+            }
+        }
+        catch (FileNotFoundException f){
+            //TODO print error message
+        }
+        catch (IOException e){
+            //TODO print error message
+        }
+
+        finally{
+            if (b != null){
+                try {
+                    b.close();
+                } catch (IOException io) {
+                    //TODO print message
+                }
+            }
+        }
+
+        //return array;
+
+        //TODO Store the worker objects in an array and return that array
     }
 }
