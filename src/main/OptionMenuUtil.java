1fef2822c5b45041feb6ba016f594f8c2030f6e1
Khoi <kal038@bucknell.edu>
2020-04-21 14:12:15 -0400

    option menu skeleton

--- src/main/OptionMenuUtil.java
+++ src/main/OptionMenuUtil.java
@@ -25,11 +25,22 @@ public class OptionMenuUtil {
 
     public static void printMenu() {
         System.out.println("Welcome to the database explorer!");
-        System.out.println("Please enter the name of your database file (.csv extension included): ");
+        System.out.println("Please enter the name of your database file, or press Enter to process default file (.csv extension included): ");
         Scanner in = new Scanner(System.in);
         String fileName = in.nextLine();
+        // a default option
+        if (fileName.equals("")) {fileName = "defDB.csv";}
+        // the option menu
+        System.out.println("Processing file [" + fileName + "]");
         System.out.println("-----------------OPTION MENU--------------------");
-
+        System.out.println("1. Query Database Data");
+        System.out.println("2. Sort Database Data");
+        System.out.println("3. Modify Date (Skip ahead in time)");
+        System.out.println("4. Undo Last Change");
+        System.out.println("5: Save Changes");
+        System.out.println("0: Exit Application");
+        // user's choice
+        String usersChoice = in.nextLine();
 
 
     }
