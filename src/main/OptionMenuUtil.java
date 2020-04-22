47ed1ea7ac3ca03b2d8960af79389ec42e3dfc09
Khoi <kal038@bucknell.edu>
2020-04-22 19:37:23 -0400

    minor changes to the Menu

--- src/main/OptionMenuUtil.java
+++ src/main/OptionMenuUtil.java
@@ -23,7 +23,13 @@ import java.util.Scanner;
 
 public final class OptionMenuUtil {
 
+
     public static void printMenu() {
+        String option1 = "1. Query Database Data\n";
+        String option2 = "2. Sort Database Data\n";
+        String option3 = "3. Modify Date (Skip ahead in time)\n";
+        String option4 = "4. Undo Last Change\n";
+        String option5 = "5: Save Changes\n";
         System.out.println("Welcome to the database explorer!");
         System.out.println("Please enter the name of your database file, or press Enter to process default file (.csv extension included): ");
         Scanner in = new Scanner(System.in);
@@ -33,18 +39,18 @@ public final class OptionMenuUtil {
         // the option menu
         System.out.println("Processing file [" + fileName + "]");
         System.out.println("-----------------OPTION MENU--------------------");
-        System.out.println("1. Query Database Data");
-        System.out.println("2. Sort Database Data");
-        System.out.println("3. Modify Date (Skip ahead in time)");
-        System.out.println("4. Undo Last Change");
-        System.out.println("5: Save Changes");
+        System.out.printf("%s", option1);
+        System.out.printf("%s", option2);
+        System.out.printf("%s", option3);
+        System.out.printf("%s", option4);
+        System.out.printf("%s", option5);
         System.out.println("0: Exit Application");
         // user's choice
         System.out.printf("Please choose an option above: ");
         String usersChoice = in.nextLine();
-        System.out.println("You have chosen option "+usersChoice);
+        System.out.println("You have chosen option "+ usersChoice);
 
 
     }
-    
+
 }
